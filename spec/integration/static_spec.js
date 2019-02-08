5203a58922c5561785a3cd9004371dc0e4f36c5e
Nick <nbjames2@gmail.com>
2019-02-07 20:53:40 -0500

    scaffolding complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,9 +6,10 @@ describe("routes : static", () => {
 
     describe("GET /", () => {
 
-        it("should return a status code of 200", (done) => {
+        it("should return a status code of 200 and have 'Welcome to Music Notifier in the body of the response", (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
+                expect(body).toContain("Welcome to Music Notifier");
                 done();
             });
         });
